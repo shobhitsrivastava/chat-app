f7b5ccae30392cdff8e5eedd2e573f01b13b4daa
shobhitsrivastava <shobhit@gatech.edu>
2016-12-25 17:49:57 -0500

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -17,6 +17,17 @@ io.on('connection', (socket) => {
 
     var time = new Date();
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user has joined',
+        createdAt: new Date().getTime()
+    });
+
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
@@ -24,7 +35,12 @@ io.on('connection', (socket) => {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        })
+        });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', (socket) => {
