7ad42f1e2e44017836b3772b37899b6019e76bd0
shobhitsrivastava <shobhit@gatech.edu>
2016-12-25 23:36:50 -0500

    Add geolocation support via geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,11 +1,20 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
         var message = generateMessage('Shobhit', 'Hi!');
         expect(message.from).toBe('Shobhit');
         expect(message.text).toBe('Hi!');
+        expect(message.createdAt).toBeA('number');
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var message = generateLocationMessage('Shobhit', 1, 1);
+        expect(message.from).toBe('Shobhit');
+        expect(message.url).toBe('https://www.google.com/maps?q=1,1');
         expect(message.createdAt).toBeA('number');
     });
 });
